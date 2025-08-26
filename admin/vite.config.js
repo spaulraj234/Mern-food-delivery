c2737d86723d7015fd9c65031406b8561467fd6a
spaulraj234 <scute8635@gmail.com>
2025-08-26 16:04:58 +0530

    update all

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mern-food-delivery/', // match the subdirectory
   plugins: [react()],
 })
+;
