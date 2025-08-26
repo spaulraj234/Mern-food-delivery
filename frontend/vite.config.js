90c70d986478b731cc9d36f800901d38825a9d7d
spaulraj234 <scute8635@gmail.com>
2025-08-26 15:30:08 +0530

    added.app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,22 +4,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mern-food-delivery/', // match the subdirectory
   plugins: [react()],
-  base: '/Mern-food-delivery/',
+})
+;
 
 
-
-   build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
-
-
-  }
-});
-
-
+  
 
 
    
